c778666f465909e22a416ce454e4e1b2c2af90b1
Cody-Price <ctprice11@gmail.com>
2018-11-06 09:51:26 -0700

    Local Remote sync after fatal error

---
+++ src/Header.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './_Header.css';
+
+export default class Header extends Component {
+
+  
+  render() {
+    return (
+      <header><h1>Pro-Flash</h1></header>
+    )
+  }
+}
